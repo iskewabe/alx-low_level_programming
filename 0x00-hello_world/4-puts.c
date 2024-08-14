7655cd13e859969d27f60dbe32d7e6a02205dd38
Eskinder <eskewabe@gmail.com>
2024-08-14 07:33:49 -0700

    first C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: returns 0
+ */
 
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
